feae9f51f10fb81d03aff847ca7322af1a2a09a4
Edwar Sanchez <edwar.s98@hotmail.com>
2023-12-22 11:49:54 -0500

    Se agrega el ejercicio Plus Minus

---
+++ src/main/java/PlusMinus.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class PlusMinus {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        double positiveNumbers = 0, negativeNumbers = 0, zeroNumbers = 0;
+
+        for (int number : arr) {
+            if(number == 0) zeroNumbers++;
+            else if(number > 0) positiveNumbers++;
+            else negativeNumbers++;
+        }
+
+        System.out.println(positiveNumbers > 0 ? (positiveNumbers / arr.size()) : 0);
+        System.out.println(negativeNumbers > 0 ? (negativeNumbers / arr.size()) : 0);
+        System.out.println(zeroNumbers > 0 ? (zeroNumbers / arr.size()) : 0);
+    }
+
+}
