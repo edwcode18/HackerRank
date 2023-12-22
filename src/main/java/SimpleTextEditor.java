19bef4402b04960bc684c9c68678bb5be08b9315
Edwar Sanchez <edwar.s98@hotmail.com>
2023-12-22 10:45:34 -0500

    Se realiza la creación de este proyecto para almacenar todos los ejercicios de Hacker Rank en Java, y se inicia creando el ejercicio de Simple Text Editor

---
+++ src/main/java/SimpleTextEditor.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SimpleTextEditor {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int Q = sc.nextInt();
+        StringBuilder S = new StringBuilder();
+        Stack<String> prevOperations = new Stack<>();
+        int k = 0;
+        for (int i = 0; i < Q; i++) {
+            int operation = sc.nextInt();
+            switch(operation){
+                case 1:
+                    String W = sc.next();
+                    prevOperations.push(S.toString());
+                    S.append(W);
+                    break;
+                case 2:
+                    k = sc.nextInt();
+                    prevOperations.push(S.toString());
+                    int len = S.length();
+                    String deleted = S.substring(len - k);
+                    S.setLength(len - k);
+                    break;
+                case 3:
+                    k = sc.nextInt();
+                    System.out.println(S.charAt(k - 1));
+                    break;
+                case 4:
+                    if(!prevOperations.isEmpty()){
+                        S = new StringBuilder(prevOperations.pop());
+                    }
+                    break;
+            }
+        }
+        sc.close();
+    }
+}
