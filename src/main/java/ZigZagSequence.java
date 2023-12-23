bd2505bf1d19eb86101c9d164691de23d7e21089
Edwar Sanchez <edwar.s98@hotmail.com>
2023-12-23 10:01:16 -0500

    Se realiza el ejercicio Zig Zag Secuence

---
+++ src/main/java/ZigZagSequence.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZigZagSequence {
+    public static void main (String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int test_cases = kb.nextInt();
+        for(int cs = 1; cs <= test_cases; cs++){
+            int n = kb.nextInt();
+            int a[] = new int[n];
+            for(int i = 0; i < n; i++){
+                a[i] = kb.nextInt();
+            }
+            findZigZagSequence(a, n);
+        }
+    }
+    public static void findZigZagSequence(int [] a, int n){
+        Arrays.sort(a);
+        int mid = (n - 1)/2;  // corregir la fórmula para encontrar el indice medio
+        int temp = a[mid];
+        a[mid] = a[n - 1];
+        a[n - 1] = temp;
+
+        int st = mid + 1;
+        int ed = n - 2; // Corregir el indice final
+        while(st <= ed){ // corregir la condicion de bucle
+            temp = a[st];
+            a[st] = a[ed];
+            a[ed] = temp;
+            st = st + 1;
+            ed = ed - 1; // decrementar el índice
+        }
+        for(int i = 0; i < n; i++){
+            if(i > 0) System.out.print(" ");
+            System.out.print(a[i]);
+        }
+        System.out.println();
+    }
+}
