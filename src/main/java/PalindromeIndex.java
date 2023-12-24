60817e976c215e8d30ae46c69b3c0c581503ffbb
Edwar Sanchez <edwar.s98@hotmail.com>
2023-12-24 08:39:52 -0500

    Se realiza el ejercicio Palindrome Index

---
+++ src/main/java/PalindromeIndex.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class PalindromeIndex {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = palindromeIndex(s);
+
+                System.out.println(String.valueOf(result));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
+    public static int palindromeIndex(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                if (isPalindrome(s, left + 1, right)) {
+                    return left;
+                } else if (isPalindrome(s, left, right - 1)) {
+                    return right;
+                }
+                return -1;
+            }
+            left++;
+            right--;
+        }
+        return -1;
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
